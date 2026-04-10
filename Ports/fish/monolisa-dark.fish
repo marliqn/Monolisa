cd57c20894f966e00918d922bd154710b61ef575
Alex <alxnem4enko@gmail.com>
2026-04-10 21:43:03 +0300

    Create the directory for fish shell colors

---
+++ Ports/fish/monolisa-dark.fish
@@ -0,0 +1,35 @@
+# Base
+set -g fish_color_normal              E8DCC0
+set -g fish_color_command             6E7F87
+set -g fish_color_keyword             8C5A6B
+set -g fish_color_quote               7A8B5A
+set -g fish_color_redirection         C69A5B
+set -g fish_color_end                 B5655D
+set -g fish_color_error               A14F3F
+set -g fish_color_param               E0B97A
+set -g fish_color_comment             7E7466
+set -g fish_color_selection           --background=3A2F28
+set -g fish_color_search_match        --background=C69A5B
+set -g fish_color_operator            8C5A6B
+set -g fish_color_escape              C69A5B
+set -g fish_color_autosuggestion      7E7466
+set -g fish_color_valid_path          7A8B5A
+
+# Completion Pager
+set -g fish_pager_color_progress      7E7466
+set -g fish_pager_color_prefix        6E7F87
+set -g fish_pager_color_completion    E8DCC0
+set -g fish_pager_color_description   B8AA94
+set -g fish_pager_color_selected_background --background=231C18
+
+# UI / Cursor
+set -g fish_color_cursor              D0A060
+set -g fish_color_cwd                 C69A5B
+set -g fish_color_user                7A8B5A
+set -g fish_color_host                6E7F87
+set -g fish_color_host_remote         A14F3F
+
+# Status
+set -g fish_color_status              A14F3F
+set -g fish_color_cancel              A14F3F
+
